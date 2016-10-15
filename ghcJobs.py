e424df505045eae46e4c263c72d17d8b01c02e00
tamblc <tamblc@rpi.edu>
2016-10-14 20:21:25 -0400

    Fixed API Call

--- ghcJobs.py
+++ ghcJobs.py
@@ -5,11 +5,12 @@
 # Key:	dPObtWeMnBw
 
 
-import urllib, urllib2, sys,csv, os
+import urllib, urllib2, sys,csv, os, json
 
 
 import urllib2, sys
-from BeautifulSoup import BeautifulSoup
+
+import time
 
  
 
@@ -18,18 +19,30 @@ def main():
 		reader = csv.reader(infile)
 		mydict = {rows[0]:rows[1] for rows in reader}
 
-	# print mydict
-	query_args = { 'name': mydict.keys()[0]}	
-	data = urllib.urlencode(query_args)
-	print data
+	final_dict = dict()
+	count = 0
+	for key in mydict.keys():
+		if count > 50:
+			print "Sleepy time!, The API has limits and you just found one ;)"
+			time.sleep(10)
+			count -= 50
 
-	url = "http://api.glassdoor.com/api/api.htm?t.p=99969&t.k=dPObtWeMnBw&userip=8.28.178.133&useragent=Mozilla&format=json&v=1&action=employers&"
-	hdr = {'User-Agent': 'Mozilla/5.0'}
-	req = urllib2.Request(url,data,headers=hdr)
-	response = urllib2.urlopen(req)
-	soup = BeautifulSoup(response)
+		CompanyName = key.strip()
+		query_args = {'action':'employers', 'keyword': CompanyName, 'ps':'1'}	
+		data = urllib.urlencode(query_args)
 
-	print soup
+		url = "http://api.glassdoor.com/api/api.htm?t.p=99969&t.k=dPObtWeMnBw&userip=129.161.129.75&useragent=Mozilla&format=json&v=1"
+		hdr = {'User-Agent': 'Mozilla/5.0'}
+		req = urllib2.Request(url,data,headers=hdr)
+		response = urllib2.urlopen(req)
+		response_dict = dict(json.loads(response.read()))['response']
+		response_dict['boothNum'] = mydict[key]
+
+		final_dict[key]= response_dict 
+		count += 1
+
+	print response_dict
+
 
 
 
