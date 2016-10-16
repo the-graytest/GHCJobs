6df316d42e29650f46884160cd5779eaeec9e782
tamblc <tamblc@rpi.edu>
2016-10-16 17:31:11 -0400

    Updated README, polished script, and added final company list

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 This project is a small script to populate a CSV of all of the companies attending Grace Hopper with glassdoor data to help attendees find their perfect match!
+
+## Usage
+
+Replace the PID, KEY, and USER_IP fields in the url in ghcJobs.py then run via command line:
+```
+python ghcJobs.py 
+```
+to populate FinalCompanyList.csv
--- ghcJobs.py
+++ ghcJobs.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 
-# Partner ID:	99969
-# Key:	dPObtWeMnBw
+# Partner ID:	PID
+# Key:	KEY
 
 
 import urllib, urllib2, sys,csv, os, json
-
+from BeautifulSoup import BeautifulSoup
 
 import urllib2, sys
 
@@ -19,29 +19,94 @@ def main():
 		reader = csv.reader(infile)
 		mydict = {rows[0]:rows[1] for rows in reader}
 
-	final_dict = dict()
+	final = []
+	fields = []
 	count = 0
 	for key in mydict.keys():
-		if count > 50:
-			print "Sleepy time!, The API has limits and you just found one ;)"
-			time.sleep(10)
-			count -= 50
+		if count > 10:
+			time.sleep(5)
+			count -= 10
+			
 
 		CompanyName = key.strip()
 		query_args = {'action':'employers', 'keyword': CompanyName, 'ps':'1'}	
 		data = urllib.urlencode(query_args)
 
-		url = "http://api.glassdoor.com/api/api.htm?t.p=99969&t.k=dPObtWeMnBw&userip=129.161.129.75&useragent=Mozilla&format=json&v=1"
+		url = "http://api.glassdoor.com/api/api.htm?t.p=PID&t.k=KEY&userip=USER_IP&useragent=Mozilla&format=json&v=1"
 		hdr = {'User-Agent': 'Mozilla/5.0'}
 		req = urllib2.Request(url,data,headers=hdr)
-		response = urllib2.urlopen(req)
-		response_dict = dict(json.loads(response.read()))['response']
-		response_dict['boothNum'] = mydict[key]
+		try: 
+			response = urllib2.urlopen(req)
+			response_dict = dict(json.loads(response.read()))['response']
+		except:
+			print key, sys.exc_info()[0] 
+			pass
 
-		final_dict[key]= response_dict 
+		print response_dict
+		if len(response_dict["employers"]) >0:
+			clean_response = response_dict["employers"][0]	
+
+		try:
+			clean_response.pop('featuredReview')
+		except:
+			pass
+		try:
+			clean_response.pop('squareLogo')
+		except:
+			pass
+		try:
+			clean_response.pop('exactMatch')
+		except:
+			pass
+		try:
+			clean_response.pop('ceo')
+		except:
+			pass
+		try:
+			clean_response.pop('sectorId')
+		except:
+			pass		
+		try:
+			clean_response.pop('id')
+		except:
+			pass		
+		try:
+			clean_response.pop('industryId')
+		except:
+			pass
+		try:
+			clean_response.pop('parentEmployer')
+		except:
+			pass
+
+
+		clean_response['boothNum'] = mydict[key]
+
+
+		final.append(clean_response) 
+
+
+		
+		
 		count += 1
 
-	print response_dict
+
+	new_final = list()
+
+	for item in final:
+		if item not in new_final:
+			new_final.append(item)
+
+	final = new_final
+
+	print final
+	fields = final[0].keys()
+	fieldsRow = {key: key for key in fields}
+	with open('FinalCompanyList.csv', 'wb') as csv_file:
+		writer = csv.DictWriter(csv_file, fields)
+		writer.writerow(fieldsRow)
+		for row in final:
+			writer.writerow(row)
 
 
 
